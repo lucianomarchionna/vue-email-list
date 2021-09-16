297d241a312e559d90f8cab0c749b353fc4d6809
Luciano <lucco6823@gmail.com>
2021-09-16 16:52:07 +0200

    Creazione 10 mail random

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 Vue.config.devtools = true;
 const app = new Vue({
     el: '#root',
-    data: {}
+    data: {
+        mails: [],
+    },
+
+    mounted(){
+        for(let i = 0; i < 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((result) => {
+            this.mails.push(result.data.response);
+        })
+        }    
+    }
 })
