27637cd95a87decab8e8ae78ef8a144f4a010d5a
Luciano <lucco6823@gmail.com>
2021-09-16 17:16:06 +0200

    Bonus

--- main.js
+++ main.js
@@ -6,11 +6,16 @@ const app = new Vue({
     },
 
     mounted(){
+        let tmp = [];
         for(let i = 0; i < 10; i++){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((result) => {
-            this.mails.push(result.data.response);
-        })
-        }    
+                tmp.push(result.data.response);
+
+                if(tmp.length == 10){
+                    this.mails = tmp;
+                }  
+            })   
+        } 
     }
 })
